86f2706ca7b68c89e2a0fb1a4d65efa1b9deea26
jonathannicolas <jonathan.s.nicolas@gmail.com>
2018-10-20 12:24:17 -0700

    add editing movie

--- mongoose-movies/routes/movies.js
+++ mongoose-movies/routes/movies.js
@@ -83,34 +83,52 @@ router.post('/:id/delete', function (req, res, next) {
         });
 });
 
-// /* Edit get specific id */
+/* Edit get specific id */
 
-// router.get('/:id/edit', function (req, res, next) {
-//     const id = req.params.id;
-//     Celebrity.findById(id)
-//         .then(celebrity => {
-//             res.render('celebrities/edit', {celebrity});
-//         })
-//         .catch(error => {
-//             next(error);
-//         });
-// });
+router.get('/:id/edit', function (req, res, next) {
+    const id = req.params.id;
+    Movie.findById(id)
+        .then(movie => {
+            res.render('movies/edit', {movie});
+        })
+        .catch(error => {
+            next(error);
+        });
+});
 
-// /* Edit a specific id */
+/* Edit a specific id */
 
-// router.post('/:id', function (req, res, next) {
-//     const obj = req.body;
-//     const id = req.params.id;
-//     console.log(obj)
-//     Celebrity.findByIdAndUpdate(id, obj)
-//     .then(celebrity => {
-//         res.redirect('/celebrities');
-//     })
-//     .catch(err => {
-//         console.error('An error occured', err);
-//         res.redirect('/celebrities/<%= id %>/edit');
-//     });
-// });
+router.post('/:id', function (req, res, next) {
+    // let {body} = req.body
+    // console.log(body)
+    const id = req.params.id;
+
+    let showtimes = []
+    if (req.body.afternoonTime) showtimes.push(req.body.afternoonTime)
+    if (req.body.morningTime) showtimes.push(req.body.morningTime)
+
+    let stars = []
+    if (req.body.femaleActor) stars.push(req.body.femaleActor)
+    if (req.body.maleActor) stars.push(req.body.maleActor)
+
+    // let showtimes = [req.body.afternoonTime]
+    const obj = {
+        stars: stars,
+        showtimes: showtimes,
+        title: req.body.title,
+        director: req.body.director,
+        image: req.body.image,
+        description: req.body.description
+    }
+    Movie.findByIdAndUpdate(id, obj)
+    .then(movie => {
+        res.redirect('/movies');
+    })
+    .catch(err => {
+        console.error('An error occured', err);
+        res.redirect('/celebrities/<%= id %>/edit');
+    });
+});
 
 
 
